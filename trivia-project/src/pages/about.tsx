d7143af2f5f4fd3004202a33b58a7c084ea132d3
thiagond-code <131370555+thiagond-code@users.noreply.github.com>
2023-05-03 17:32:17 +0000

    feat: Add about page structure and install Tailwind Typography

--- trivia-project/src/pages/about.tsx
+++ trivia-project/src/pages/about.tsx
@@ -1,3 +1,15 @@
 export default function About() {
-    return <div></div>
+    return (
+        <main className='flex flex-col items-center'>
+            <section className='container mx-auto flex flex-col items-center gap-4 prose'>
+                <h1 className='text-3xl font-bold'>About the Quiz</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Officiis, velit! Enim error officia corporis praesentium fugiat
+                    in, rerum incidunt placeat, eaque, laudantium amet alias
+                    inventore! Ut nostrum numquam et expedita.
+                </p>
+            </section>
+        </main>
+    )
 }
